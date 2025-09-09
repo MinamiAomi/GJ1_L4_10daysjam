415cff0d8032d6125386308ea132135239a21853
MinamiAomi <k022g0040@g.neec.ac.jp>
2025-09-09 16:25:36 +0900

    かべ速度上昇＋プレイヤーふわふわ

--- Game/Wall.h
+++ Game/Wall.h
@@ -15,6 +15,8 @@ public:
     static constexpr float kCameraOffsetZ = -44.0f;
     static constexpr float kCameraRotateY = 33.0f;
     static constexpr float kInitializeSpeed = 0.05f;
+    static constexpr float kSpeedFactor = 0.0001f;
+    static constexpr float kSpeedLimit = 0.5f;
     static constexpr float kBurstDistance = 100.0f;
     static constexpr float kBurstEndDistance = 75.0f;
     static constexpr float kBurstSpeed = 100.0f;
